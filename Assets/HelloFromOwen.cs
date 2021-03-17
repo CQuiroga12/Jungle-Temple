5022888c6c45f75eada2785e264e6014eb60aabb
eddieshores <edshores@davidson.edu>
2021-03-17 15:37:29 -0400

    Added hello to Start()

--- Assets/HelloFromOwen.cs
+++ Assets/HelloFromOwen.cs
@@ -7,7 +7,7 @@ public class HelloFromOwen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Hello from Eddie
     }
 
     // Update is called once per frame
