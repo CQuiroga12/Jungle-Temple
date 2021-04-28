c91d8817dd6472f7ac10f2081e165c66af481d27
CQuiroga12 <77950094+CQuiroga12@users.noreply.github.com>
2021-04-27 20:07:27 -0400

    Revert "2nd character error fixed, still have error on controls and camera"
    This reverts commit 80a405dd7e5f30f62ab734d275df4c560c8380a6.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,13 +6,10 @@ using Photon.Pun;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject PlayerPrefab;
     public GameObject GameCanvas;
     public GameObject SceneCamera;
-<<<<<<< Updated upstream
     public int PlayerCount = 0;
-=======
-    public int PlayerCount;
->>>>>>> Stashed changes
 
     private void Awake()
     {
@@ -21,7 +18,6 @@ public class GameManager : MonoBehaviour
     public void SpawnPlayer()
     {
         float randomValue = Random.Range(-1f, 1f);
-<<<<<<< Updated upstream
         if (PlayerCount == 0){
             Debug.Log("Player1 Spawning");
             PhotonNetwork.Instantiate("EC_Player1", new Vector2(this.transform.position.x * randomValue, this.transform.position.y), Quaternion.identity, 0);
@@ -33,25 +29,6 @@ public class GameManager : MonoBehaviour
             Debug.Log("Player2 Spawning");
             PhotonNetwork.Instantiate("EC_Player2", new Vector2(this.transform.position.x * randomValue, this.transform.position.y), Quaternion.identity, 0);
             Debug.Log("Player2 Spawned");
-=======
-
-        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
-        {
-            Debug.Log("Preparing to Spawn Player 1");
-            PhotonNetwork.Instantiate("EC_Player1", new Vector2(this.transform.position.x * randomValue, this.transform.position.y), Quaternion.identity, 0);
-            Debug.Log("Spawned Player 1");
-            GameCanvas.SetActive(false);
-            SceneCamera.SetActive(false);
-            PlayerCount++;
-
-        } else{
-            Debug.Log("Preparing to Spawn Player 2");
-            PhotonNetwork.Instantiate("EC_Player2", new Vector2(this.transform.position.x * randomValue, this.transform.position.y), Quaternion.identity, 0);
-            Debug.Log("Spawned Player 2");
-            GameCanvas.SetActive(false);
-            SceneCamera.SetActive(false);
-            PlayerCount++;
->>>>>>> Stashed changes
         }
     }
 
